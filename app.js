2cbe2ea59ed2239fe4a8a9c548f00485f27af5cd
Wolf <d.wolftripp@gmail.com>
2022-09-21 13:20:00 -0700

    first problem

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('sanity check')
+// 1. Sum of All Arguments
+// sum(1, 2, 3) // => 6
+// sum(8, 2) // => 10
+// sum(1, 2, 3, 4, 5) // => 15
+
+function sum(){
+  total = 0
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i]
+  }
+  return total
+}
+
+console.log("1.", sum(1, 2, 3))
+console.log("2.", sum(8, 2))
+console.log("3.", sum(1, 2, 3, 4, 5))
