42a1ba6cfa4f1dc92efeb3d30bbffe0b9d8b3732
Wolf <d.wolftripp@gmail.com>
2022-09-22 13:25:06 -0700

    problem 3

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function sum(){
 // console.log("2.", sum(8, 2))
 // console.log("3.", sum(1, 2, 3, 4, 5))
 
-// ______________________________________________
+// ______________________________________________ //
 
 // 2. String Ends With?
   // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
@@ -24,7 +24,21 @@ function sum(){
     return str.endsWith(ending)
   }
 
-  console.log("1.", solution('abc', 'bc'))
-  console.log("2.", solution('abc', 'd'))
-  console.log("3.", solution('abcde', 'cde'))
-  console.log("4.", solution("abcde", "abc"))
+  // console.log("1.", solution('abc', 'bc'))
+  // console.log("2.", solution('abc', 'd'))
+  // console.log("3.", solution('abcde', 'cde'))
+  // console.log("4.", solution("abcde", "abc"))
+
+  // ______________________________________________ //
+
+// 3. Detect Pangram
+  //A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+  // Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+  function isPangram(string) {
+    var regex = /([a-z])(?!.*\1)/g;
+    return (string.match(regex) || []).length === 26;
+}
+
+console.log("1.", isPangram("The quick brown fox jumps over the lazy dog."))
+console.log("2.", isPangram("This is not a panogram."))
